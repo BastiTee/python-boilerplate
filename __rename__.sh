b7f11e6c52eb548120a7f589fac01bd7081f88c3
Basti Tee <basti.tee@posteo.de>
2020-06-24 09:52:49 +0200

    Add script to rename project

---
+++ __rename__.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd "$( cd "$( dirname "$0" )"; pwd )"
+
+show_help() {
+    cat << EOF
+Rename 'my_module' to your preferred project name.
+
+Usage: $( basename $0 ) <NEW_NAME>
+
+Arguments:
+
+    NEW_NAME       New project name.
+
+EOF
+}
+
+if [ -z "$1" ]; then show_help; exit 1; fi
+
+mv my_module $1
+find . -type f -exec grep -l my_module {} + |\
+grep -v -e $( basename $0 ) -e ".git" |while read file
+do
+    sed -i.bak "s/my_module/$1/g" $file
+    rm -fv $file.bak
+done
